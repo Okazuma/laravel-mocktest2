d400a9898b1833f51bf031622fc661040ea32ba5
kazuma oshima <kxxxzg@gmail.com>
2024-08-10 01:57:32 +0900

    READMEの編集

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('reminder:send')->dailyAt('09:00');
+        $schedule->command('reminder:send')->dailyAt('09:00'); //毎朝９時に実行
     }
 
     /**
