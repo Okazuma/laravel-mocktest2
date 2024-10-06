d7e74e3ec6dc0105a5010d450ea644b26e36545e
kazuma oshima <kxxxzg@gmail.com>
2024-10-07 06:20:10 +0900

    cssの修正

--- src/resources/views/guest-menu.blade.php
+++ src/resources/views/guest-menu.blade.php
@@ -7,19 +7,9 @@
 @section('content')
 <div class="container">
     <div class="inner">
-
-
             <a class="home__button" href="{{route('restaurants.index')}}">Home</a>
-
-
-
             <a class="register__button" href="{{route('register')}}">Registration</a>
-
-
-
             <a class="login__button" href="{{route('login')}}">Login</a>
-
-
     </div>
 </div>
 @endsection
